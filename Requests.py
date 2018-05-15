55567bc6fbb96187f84220d2079b978d895a81fc
LudoHazred <ludovic.gros2@gmail.com>
2018-05-15 15:54:17 +0200

    Added requests.py to interact with the api with the module requests

---
+++ Requests.py
@@ -0,0 +1,20 @@
+import requests
+
+payload = {
+    'action': 'process',
+    'tagtype_0': 'categories',
+    'tag_contains_0': 'contains',
+    'tag_0': '', #parametres to choose
+    'sort_by': 'unique_scans_n',
+    'page_size': 100,
+    'countries': 'France',
+    'json': 1,
+    'page': 1,
+    }
+
+r = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=payload)
+
+r.json
+
+print r.url
+
