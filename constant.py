a3f6b411a56df0b17c314048618d46a6e5f4bcae
LudoHazred <ludovic.gros2@gmail.com>
2018-05-30 16:20:15 +0200

    Add constant.py and sqlalchemy.py for respectively security and tests, modified requests.py to make it to be the class document and added bddcreation.sql that allow to create the architecture of the database

---
+++ constant.py
@@ -0,0 +1,4 @@
+CATEGORIES_SELEC = sodas
+DB_NAME = purbeurre
+DB_USER = pbadmin
+DB_PWD = admin
---
+++ sqlalchemy.py
@@ -0,0 +1,5 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql://pbadmin:admin@localhost/purbeurre')
+
