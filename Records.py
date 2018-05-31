6fede5fb833f5fae5c0ddb823c48d678e7d33133
LudoHazred <ludovic.gros2@gmail.com>
2018-05-31 18:00:04 +0200

    Deleted records.py, requests.py and sqlalchemy.py because they are not usefull anymore

--- Records.py
+++
@@ -1,4 +0,0 @@
-import records
-
-db = records.Database('mysql://pbadmin:admin@localhost/purbeurre')
-rows = db.query('USE purbeurre;')
--- sqlalchemy.py
+++
@@ -1,5 +0,0 @@
-import sqlalchemy
-from sqlalchemy import create_engine
-
-engine = create_engine('mysql://pbadmin:admin@localhost/purbeurre')
-
