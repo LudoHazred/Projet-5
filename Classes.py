6574a96c7233717839b344257c16b1ad3757dcf7
LudoHazred <ludovic.gros2@gmail.com>
2018-06-19 17:40:11 +0200

    added def in classes, writed some test in test.py and renamed code json

--- Classes.py
+++ Classes.py
@@ -1,36 +1,45 @@
 import requests
 import json
-import records
+import mysql.connector
 from constant import *
 
-def __init__(self, categories_selec):
-    '''Initialization username, password, database name and categories'''
-    self.categories_selec = CATEGORIES_SELEC
-    self.db_name = DB_NAME
-    self.db_pwd = DB_PWD
-    self.db_user = DB_USER
+class OpenFood:
 
-def connect_db(self):
-    '''Connect to database'''
-    self.db = records.Database('mysql://{}:{}@localhost/{}'.format(self.db_user, self.db_pwd, self.db_name))
-    self.db.query('USE {};'.format(self.db_name))
+    def __init__(self):
+        '''Initialization username, password, database name and categories'''
+        self.categories_selec = CATEGORIES_SELEC
+        self.db_name = DB_NAME
+        self.db_pwd = DB_PWD
+        self.db_user = DB_USER
 
-def get_data(self):
-    self.payload = {
-        'action': 'process',
-        'tagtype_0': 'categories', #which subject is selected (categories)
-        'tag_contains_0': 'contains', #contains or not
-        'tag_0': '{}'.format(categories_selec), #parameters to choose
-        'sort_by': 'unique_scans_n',
-        'page_size': 50,
-        'countries': 'France',
-        'json': 1,
-        'page': 1
-        }
-    r = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=self.payload)
-    self.data = r.json()
+        '''connection to the database'''
+        self.db = mysql.connector.connect(user='{}'.format(self.db_user), password='{}'.format(self.db_pwd), host='localhost', database='{}'.format(self.db_name))
+        self.cursor = db.cursor()
 
-def research(self, product):
-	self.db.query('')
+    def get_category(self):
+        '''get categories from the URL API'''
+        r_cat = requests.get('https://fr.openfoodfacts.org/categories&json=1')
+        cat_json = r_cat.json()
+        add_category = ("INSERT INTO Category" "category" "VALUES (%s)")
+        self.cursor.execute(add_category, cat_json)
+        db.commit()
 
-db = records.Database('mysql://pbadmin:admin@localhost/purbeurre'.format(self.db_user, self.db_pwd, self.db_name))
+    def get_food(self, categories_selec):
+        '''Parameters and request to the URL API'''
+        self.payload = {
+            'action': 'process',
+            'tagtype_0': 'categories', #which subject is selected (categories)
+            'tag_contains_0': 'contains', #contains or not
+            'tag_0': '{}'.format(categories_selec), #categories to choose
+            'sort_by': 'unique_scans_n',
+            'page_size': 50,
+            'countries': 'France',
+            'json': 1,
+            'page': 1
+            }
+
+        r_food = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=self.payload)
+        food_json = r_food.json()
+        add_food = ("INSERT INTO Food" "")
+        self.cursor.execute(add_food, food_json)
+        db.commit()
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import records
+import mysql.connector
 from constant import *
 
 categories_selec = CATEGORIES_SELEC
@@ -8,8 +8,30 @@ db_name = DB_NAME
 db_pwd = DB_PWD
 db_user = DB_USER
 
-db = records.Database('mysql://{}:{}@localhost'.format(db_user, db_pwd))
-queue = db.query('USE {};'.format(db_name))
+'''Connection to the database'''
+db = mysql.connector.connect(user='{}'.format(db_user), password='{}'.format(db_pwd), host='localhost', database='{}'.format(db_name))
+cursor = db.cursor()
+
+'''For Shell'''
+db = mysql.connector.connect(user='pbadmin', password='admin', host='localhost', database='purbeurre')
+
+'''request for categories'''
+r_cat = requests.get('https://fr.openfoodfacts.org/categories&json=1')
+data_json = r_cat.json()
+serial_data = json.dumps(data_json)
+add_category = ("INSERT INTO Category" "(category)" "VALUES (%(name)s)")
+cursor = db.cursor()
+cursor.execute("""INSERT INTO Category(category) VALUES(%(category)s)""", data_json)
+cursor.execute(add_category, serial_data)
+db.commit()
+cursor.close()
+db.close()
+
+'''insert values into the table Category'''
+add_category = ("INSERT INTO Category"
+    "(category)"
+    "VALUES (%s)"
+    )
 
 payload = {
     'action': 'process',
